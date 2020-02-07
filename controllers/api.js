d8a5d138641d0da517a8a547a8deee86c45b4b8a
Dima Semko <dimaisprogrammer@gmail.com>
2020-02-07 12:36:43 +0300

    error handling

--- controllers/api.js
+++ controllers/api.js
@@ -4,9 +4,9 @@ const { comparePassport } = require('../logic/passportComparator');
 
 class Api {
     static async parsePassportPdf(req, res) {
-        const { passport_url } = req.body;
-        if (!passport_url) {
-            res.status(500).json({ err: 'u didnt pass required params: passport_url'});
+        const { passport_url, name, last_name, patronymic, series, number, issued_by, issue_date, department_code  } = req.body;
+        if (!passport_url || !name || !last_name || !patronymic || !series || !number || !issued_by || !issue_date || !department_code ) {
+            res.status(500).json({ err: 'u didnt pass required params'});
             return;
         }
         const loadedFileData = await loadFileByUrl(passport_url);
